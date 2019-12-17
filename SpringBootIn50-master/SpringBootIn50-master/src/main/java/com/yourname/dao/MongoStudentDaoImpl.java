889b27b5786b0aef67a0881a0977ce81c085c5f6
deepakhk18 <42630385+deepakhk18@users.noreply.github.com>
2019-12-17 08:52:53 -0800

    Add files via upload

---
+++ SpringBootIn50-master/SpringBootIn50-master/src/main/java/com/yourname/dao/MongoStudentDaoImpl.java
@@ -0,0 +1,45 @@
+package com.yourname.dao;
+
+import com.yourname.entity.Student;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Repository
+@Qualifier("mongoData")
+public class MongoStudentDaoImpl implements StudentDao {
+
+
+    @Override
+    public Collection<Student> getAllStudents() {
+        return new ArrayList<Student>(){
+            {
+                add(new Student(1, "Mario", "Nothing"));
+                add(new Student(2,"deepak","anything"));
+                add(new Student (3,"divya","abc"));
+            }
+        };
+    }
+
+    @Override
+    public Student getStudentById(int id) {
+        return null;
+    }
+
+    @Override
+    public void removeStudentById(int id) {
+
+    }
+
+    @Override
+    public void updateStudent(Student student) {
+
+    }
+
+    @Override
+    public void insertStudentToDb(Student student) {
+
+    }
+}
